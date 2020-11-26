d37396dc45c2806eb0c730b9e6a11b719be4a874
erikjuhani <erik.kinn@gmail.com>
2020-11-26 17:29:46 +0200

    Add tests for create command

--- cmd/create_test.go
+++ cmd/create_test.go
@@ -1,11 +1,22 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	lib "github.com/libgit2/git2go/v30"
 )
+
+func contains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
 
 func TestCreateBranchCmd(t *testing.T) {
 	tests := []struct {
@@ -14,7 +25,7 @@ func TestCreateBranchCmd(t *testing.T) {
 	}{
 		{
 			name: `Command gong create branch <branchname>.
-			Should create a new branch with <branchname>`,
+				Should create a new branch with <branchname>`,
 			args: []string{"gong-branch"},
 		},
 	}
@@ -47,6 +58,151 @@ func TestCreateBranchCmd(t *testing.T) {
 			_, err = repo.Core.LookupBranch(tt.args[0], lib.BranchLocal)
 			if err != nil {
 				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestCreateFileCmd(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+	}{
+		{
+			name: `Command gong create file <pathspec>.
+				Should create a new regular file with <pathspec>.
+				e.g. "gong create file module/gongo-bongo.go"
+				Creates a file gongo-bongo.go to a directory module.
+				If directory does not exists create directory.`,
+			args: []string{"module/gongo-bongo.go"},
+		},
+	}
+	repo, err := createTestRepo()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer cleanupTestRepo(repo)
+
+	workdir := repo.Core.Workdir()
+
+	if err := os.Chdir(workdir); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			args := []string{createCmd.Name(), createFileCmd.Name()}
+			args = append(args, tt.args...)
+
+			rootCmd.SetArgs(args)
+
+			err = rootCmd.Execute()
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			filepath := fmt.Sprintf("%s%s", workdir, tt.args[0])
+
+			if _, err := os.Stat(filepath); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestCreateTagCmd(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+	}{
+		{
+			name: `Command gong create tag <tagname>. Should create a new tag <tagname>`,
+			args: []string{"v0.1.0"},
+		},
+	}
+	repo, err := createTestRepo()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer cleanupTestRepo(repo)
+
+	workdir := repo.Core.Workdir()
+
+	if err := os.Chdir(workdir); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			args := []string{createCmd.Name(), createTagCmd.Name()}
+			args = append(args, tt.args...)
+
+			rootCmd.SetArgs(args)
+
+			err = rootCmd.Execute()
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			tags, err := repo.Core.Tags.List()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !contains(tags, tt.args[0]) {
+				t.Fatal(fmt.Errorf("tag %s does not exist in tags list %q", tt.args[0], tags))
+			}
+		})
+	}
+}
+
+func TestCreateReleaseCmd(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+	}{
+		{
+			name: `Command gong create release <releasename>. Should create a new tag <releasename>`,
+			args: []string{"v0.1.0"},
+		},
+	}
+	repo, err := createTestRepo()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer cleanupTestRepo(repo)
+
+	workdir := repo.Core.Workdir()
+
+	if err := os.Chdir(workdir); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			args := []string{createCmd.Name(), createReleaseCmd.Name()}
+			args = append(args, tt.args...)
+
+			rootCmd.SetArgs(args)
+
+			err = rootCmd.Execute()
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			tags, err := repo.Core.Tags.List()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !contains(tags, tt.args[0]) {
+				t.Fatal(fmt.Errorf("release %s does not exist in tags list %q", tt.args[0], tags))
 			}
 		})
 	}
